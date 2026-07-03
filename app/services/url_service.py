c198660aad77f679233f2297d063b51f11863cc0
JoePallister <joepallister@gmail.com>
2026-07-03 20:45:06 +0100

    placeholder functions for generating url

--- app/services/url_service.py
+++ app/services/url_service.py
@@ -1,6 +1,19 @@
 def make_short_url(url: str) -> str:
-    return url
+    if not validate_url(url):
+        raise ValueError("Invalid URL")
+
+    short_url = generate_short_url(url)
+    return short_url
 
 
 def expand_short_url(short_url: str) -> str:
     return short_url
+
+
+def validate_url(url: str) -> bool:
+    return True
+
+
+def generate_short_url(url: str) -> str:
+    # Placeholder for actual short URL generation logic
+    return url
--- notes.md
+++ notes.md
@@ -1,3 +1,19 @@
 To run the api cd into app, activate the venv and run
 
 uvicorn main:app --reload
+
+Need to remember: 
+
+Use short_code as the DB index since we will be doing lookups (including uniqueness lookup) on it
+
+Table could later have 
+
+created_at
+expires_at (optional)
+click_count (optional later)
+
+On collision have chosen to generate a new code
+
+How do we redirect?
+
+URL validation
